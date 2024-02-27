15a7e288fe38d419959ceee6aa9a332a71f550a2
František Šilhán <frantisek@slhn.cz>
2024-02-27 17:19:20 +0100

    add logout route

---
+++ src/routes/logout.js
@@ -0,0 +1,18 @@
+const shared = require("../../shared");
+const express = require('express');
+const router = express.Router();
+const { isAuthenticated } = require(shared.files.middlewares);
+
+router.path = "/logout";
+
+router.get("/", isAuthenticated, (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
+module.exports = router;
