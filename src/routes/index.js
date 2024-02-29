42fd3c9a5e8cdf8e369ea683f097f2b5f8cbc08b
Mapetr <info@mapetr.moe>
2024-02-29 17:17:45 +0100

    add infinite scrolling to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ router.get("/", isAuthenticated, async (_, res, next) => {
     const { db } = require(shared.files.database);
 
     const files = await new Promise((resolve, reject) => {
-      db.all("SELECT fileName, displayName, fileSize, md5, mimeType FROM files WHERE indexFile = 1 ORDER BY added DESC",
+      db.all("SELECT fileName, displayName, fileSize, md5, mimeType FROM files WHERE indexFile = 1 ORDER BY added DESC LIMIT 35",
         (err, rows) => err ? reject(err) : resolve(rows)
       );
     });
@@ -23,6 +23,34 @@ router.get("/", isAuthenticated, async (_, res, next) => {
     }));
 
     res.render("index", { files: formattedFiles });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/api/files", async (req, res, next) => {
+  try {
+    const { db } = require(shared.files.database);
+
+    const offset = req.query.offset ? parseInt(req.query.offset, 10) : 0;
+    if (isNaN(offset) || offset <= 0) {
+      const err = new Error("Invalid offset");
+      err.status = 400;
+      throw err;
+    }
+
+    const files = await new Promise((resolve, reject) => {
+      db.all(`SELECT fileName, displayName, fileSize, md5, mimeType FROM files WHERE indexFile = 1 ORDER BY added DESC LIMIT 35 OFFSET ${offset}`,
+        (err, rows) => err ? reject(err) : resolve(rows)
+      );
+    });
+
+    const formattedFiles = files.map(file => ({
+      ...file,
+      fileSize: formatFileSize(file.fileSize)
+    }));
+
+    res.json(formattedFiles);
   } catch (err) {
     next(err);
   }
