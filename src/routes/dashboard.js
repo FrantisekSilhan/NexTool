87148bafcdcb4a2c352345e3261de1308e9ad971
Mapetr <info@mapetr.moe>
2024-02-29 22:12:23 +0100

    Feature/infinite scrolling (#7)
    * move begin transaction
    
    * add limit for video conversion
    
    * Update README.md
    
    * add maximum video convert time to config
    
    * sort files by the newest
    
    * add infinite scrolling to index
    
    * change sorting to sort based on id
    
    * add infinite scrolling to dashboard and polish the code

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -12,9 +12,9 @@ router.get("/", isAuthenticated, async (req, res, next) => {
     const { db } = require(shared.files.database);
 
     const userId = req.session.userId;
-    
+
     const files = await new Promise((resolve, reject) => {
-      db.all("SELECT fileName, displayName, fileSize, md5, mimeType FROM files WHERE owner = ?",
+      db.all("SELECT fileName, displayName, fileSize, md5, mimeType FROM files WHERE owner = ? ORDER BY id DESC LIMIT 35",
         [userId],
         (err, rows) => err ? reject(err) : resolve(rows)
       );
@@ -26,6 +26,36 @@ router.get("/", isAuthenticated, async (req, res, next) => {
     }));
 
     res.render("dashboard", { userName: req.session.username, files: formattedFiles, userId });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/api/files", isAuthenticated, async (req, res, next) => {
+  try {
+    const { db } = require(shared.files.database);
+
+    const userId = req.session.userId;
+    const offset = req.query.offset ? parseInt(req.query.offset, 10) : 0;
+    if (isNaN(offset) || offset <= 0) {
+      const err = new Error("Invalid offset");
+      err.status = 400;
+      throw err;
+    }
+
+    const files = await new Promise((resolve, reject) => {
+      db.all(`SELECT fileName, displayName, fileSize, md5, mimeType FROM files WHERE owner = ? ORDER BY added DESC LIMIT 35 OFFSET ${offset}`,
+        [userId],
+        (err, rows) => err ? reject(err) : resolve(rows)
+      );
+    });
+
+    const formattedFiles = files.map(file => ({
+      ...file,
+      fileSize: formatFileSize(file.fileSize)
+    }));
+
+    res.json(formattedFiles);
   } catch (err) {
     next(err);
   }
