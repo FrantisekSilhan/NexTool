a1ba8d92bca90716f0fe589cd462285703b98966
Mapetr <info@mapetr.moe>
2024-03-11 21:57:45 +0100

    add css from old version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "./style.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -10,13 +10,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    </head>
+    <body className={inter.className}>{children}</body>
     </html>
   );
 }
