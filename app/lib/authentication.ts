3d81e2fa68b145ed7b381ec59195f18e83bc4f52
Mapetr <info@mapetr.moe>
2024-03-13 22:48:54 +0100

    add option to register

--- app/lib/authentication.ts
+++ app/lib/authentication.ts
@@ -1,6 +1,8 @@
 'use server'
 
 import crypto from "crypto";
+import prisma from "@/lib/prisma";
+import {redirect} from "next/navigation";
 
 export async function authenticate(_currentState: unknown, formData: FormData): Promise<string> {
   console.log("Authenticating user...");
@@ -17,4 +19,68 @@ export async function authenticate(_currentState: unknown, formData: FormData): 
   }
 
   return "";
+}
+
+export async function register(_currentState: unknown, formData: FormData): Promise<string> {
+  console.log("Registering user...");
+  console.log("Form data:", formData);
+
+  let username = formData.get('username');
+  const password = formData.get('password');
+  const invite = formData.get('invite');
+
+  if (!username || typeof username !== "string") {
+    return "Username is required";
+  }
+  if (!password || typeof password !== "string") {
+    return "Password is required";
+  }
+  if (!invite || typeof invite !== "string") {
+    return "Invite is required";
+  }
+
+  username = username.toLowerCase();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      userName: username
+    }
+  });
+  if (user) {
+    return "User already exists";
+  }
+
+  const inviteCode = await prisma.invite.findUnique({
+    where: {
+      invite: invite,
+      usedBy: null
+    }
+  });
+  if (!inviteCode) {
+    return "Invalid invite code";
+  }
+
+  const salt = crypto.randomBytes(16).toString("hex");
+  const passwordHash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex");
+
+  const createdUser = await prisma.user.create({
+    data: {
+      userName: username,
+      password: passwordHash,
+      salt: salt
+    }
+  });
+
+  await prisma.invite.update({
+    where: {
+      invite: invite
+    },
+    data: {
+      usedBy: {
+        set: createdUser.id
+      }
+    }
+  });
+
+  return redirect("/dashboard");
 }
---
+++ app/register/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {register} from "@/app/lib/authentication";
+import {useFormState, useFormStatus} from "react-dom";
+
+export default function Register() {
+  const [errorMessage, dispatch] = useFormState(register, undefined)
+
+  return (
+    <div>
+      <form action={dispatch} className="form">
+        <div className="form__items">
+          <label className="form__label" htmlFor="username">Username:</label>
+          <input className="form__input" type="text" id="username" name="username" required={true}/>
+
+          <label className="form__label" htmlFor="password">Password:</label>
+          <input className="form__input" type="password" id="password" name="password" required={true}/>
+
+          <label className="form__label" htmlFor="invite">Invite code:</label>
+          <input className="form__input" type="text" id="invite" name="invite" required={true}/>
+        </div>
+        <div>
+          <p className="error">{errorMessage}</p>
+        </div>
+        <SignUpButton />
+      </form>
+      <p>Don't have an account? <a className="link" href="/register">Sign up</a></p>
+    </div>
+  );
+}
+
+function SignUpButton() {
+  const {pending} = useFormStatus();
+
+  return (
+    <button className="btn" aria-disabled={pending} type={"submit"}>Sign up</button>
+  )
+}
